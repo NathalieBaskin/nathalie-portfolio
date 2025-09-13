763ab88e9f0a7e7b3136e26ad385d6a1c412e1b8
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2025-09-13 20:47:20 +0200

    för större skärm

--- app/page.js
+++ app/page.js
@@ -3,37 +3,48 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white text-slate-900">
-      {/* HERO – bilden lämnas exakt där den är */}
-      <section className="relative flex items-center justify-center px-4 md:px-6 pt-0 pb-10 -mt-2 md:-mt-100">
+      {/* HERO – desktop-position behålls; mobil: bara bild; iPad fixar vi höjden */}
+      <section className="relative px-4 md:px-6 pt-0 pb-10 -mt-2 md:-mt-[40px] lg:-mt-[100px] overflow-x-hidden">
         {/* Wrapper runt bild + texter (så texter följer bilden) */}
-        <div className="relative mx-auto w-full max-w-[95vw] md:max-w-[1600px] translate-x-4 md:translate-x-10">
-          {/* Bild (oförändrad position) */}
+        <div className="relative mx-auto w-full max-w-[100vw] md:max-w-[1400px] lg:max-w-[1600px] xl:max-w-[1800px] 2xl:max-w-[2000px] translate-x-4 md:translate-x-10">
+          {/* Bild (oförändrad desktop-position) */}
           <Image
             src="/hero.png"
             alt="Nathalie photographer and coder"
             width={1600}
             height={800}
             className="w-full rounded-lg"
-            sizes="(min-width: 768px) 1600px, 95vw"
+            sizes="(min-width:1536px) 2000px, (min-width:1280px) 1800px, (min-width:1024px) 1600px, (min-width:768px) 90vw, 100vw"
             priority
           />
 
-          {/* Vänster text – MITTEN av bilden, lite längre ner */}
-          <h1 className="hidden md:block absolute right-[1005px] top-[70%] -translate-y-1/2 translate-y-6 text-3xl font-extrabold text-black z-10 select-none">
+          {/* Vänster text – MITTEN av bilden. GÖMS på mobil, mindre på iPad, större på stora skärmar */}
+          <h1
+            className="
+              hidden md:block absolute
+              right-[1005px] top-[70%] -translate-y-1/2 translate-y-6
+              text-2xl lg:text-3xl xl:text-5xl 2xl:text-6xl
+              font-extrabold text-black z-10 select-none
+            "
+          >
             photographer
           </h1>
 
-          {/* Höger text – flyttad in mot mitten + lite längre ner */}
-          <h1 className="hidden md:block absolute right-[110px] top-[69%] -translate-y-1/2 translate-y-6 text-3xl font-extrabold text-black z-10 select-none">
+          {/* Höger text – flyttad in mot mitten (din siffra behålls), lika responsiva storlekar */}
+          <h1
+            className="
+              hidden md:block absolute
+              right-[110px] top-[69%] -translate-y-1/2 translate-y-6
+              text-2xl lg:text-3xl xl:text-5xl 2xl:text-6xl
+              font-extrabold text-black z-10 select-none
+            "
+          >
             &lt;coder&gt;
           </h1>
         </div>
 
-        {/* Mobil: texter under bilden */}
-        <div className="md:hidden flex flex-col items-center gap-2 mt-4">
-          <h1 className="text-2xl font-bold text-black">photographer</h1>
-          <h1 className="text-2xl font-bold text-black">&lt;coder&gt;</h1>
-        </div>
+        {/* Mobil: visa bara bilden (inga texter) */}
+        {/* (Med hidden md:block på texterna ovan visas de inte på mobil) */}
       </section>
 
       {/* Projekt */}
