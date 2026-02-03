3e58f8340cadfd2558a8d4ad9991b95e22a3bfbe
NathalieBaskin <nathalie.baskin@learnet.se>
2026-02-03 18:48:25 +0100

    layout

---
+++ app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.png" sizes="32x32" />
+      <link rel="icon" href="/favicon.png" sizes="48x48" />
+      <link rel="icon" href="/favicon.png" sizes="64x64" />
+      <link rel="apple-touch-icon" href="/favicon.png" />
+    </>
+  );
+}
